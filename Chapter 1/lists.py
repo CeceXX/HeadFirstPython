4faa9d3683e4a33e4adb4494f257d133bee3695a
CeceXX <hi@cesare.io>
2016-01-01 17:42:42 +0100

    Added files - completed exercises

---
+++ Chapter 1/lists.py
@@ -0,0 +1,15 @@
+movies1 = ["The Holy Grail",
+           "The Life of Brian",
+           "The Meaning of Life",
+           "Ex-Machina"]
+movies1.insert(1, 1975)
+movies1.insert(3, 1979)
+movies1.insert(5, 1983)
+movies1.insert(7, 2014)
+ 
+movies2 = ["The Holy Grail", 1975,
+          "The Life of Brian", 1979,
+          "The Meaning of Life", 1983]
+
+for movie in movies1:
+    print(movie)
