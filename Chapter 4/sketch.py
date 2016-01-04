fc68d48ffec734b2fa56f9b4791381a769c67b6a
Cesare de Cal <hi@cesare.io>
2016-01-04 19:05:37 +0100

    Completed exercise about saving data to files

--- Chapter 4/sketch.py
+++ Chapter 4/sketch.py
@@ -1,15 +1,32 @@
 # sketch.py
 
+man = []
+other = []
+
 try:
     data = open('sketch.txt')
 
     for line in data:
         try:
             (role, line_spoken) = line.split(':', 1)
-            print(role, end='')
-            print(' said: ', end='')
-            print(line_spoken, end='')
+            line_spoken = line_spoken.strip()
+            if role == 'Man':
+                man.append(line_spoken)
+            elif role == 'Other Man':
+                other.append(line_spoken)
         except ValueError:
             pass
 except IOError:
     print('The data file is missing!')
+
+try:
+    manFile = open("manScript.txt", 'w+')
+    otherFile = open('otherManScript.txt', 'w+')
+
+    print(man, file=manFile)
+    print(other, file=otherFile)
+
+    manFile.close()
+    otherFile.close()
+except IOError:
+    print("I couldn't write to the file")
