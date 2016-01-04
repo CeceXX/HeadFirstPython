3441c47593f44b99d6be5e2d4484ac4b8b376fdb
Cesare de Cal <hi@cesare.io>
2016-01-04 13:43:40 +0100

    Completed code for Chapter 3

--- Chapter 3/sketch.py
+++ Chapter 3/sketch.py
@@ -1,10 +1,15 @@
 # sketch.py
 
-data = open('sketch.txt')
+try:
+    data = open('sketch.txt')
 
-for line in data:
-    (role, line_spoken) = line.split(':', 1)
-    print(role, end='')
-    print(' said: ', end='')
-    print(line_spoken, end='')
-
+    for line in data:
+        try:
+            (role, line_spoken) = line.split(':', 1)
+            print(role, end='')
+            print(' said: ', end='')
+            print(line_spoken, end='')
+        except ValueError:
+            pass
+except IOError:
+    print('The data file is missing!')
